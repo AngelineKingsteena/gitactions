a460a92b7bcb076769613dc2edc5597688d7f0bd
AK <angelinekingsteena@gmail.com>
2022-09-17 04:14:53 +0530

    changed scripts

--- simple.py
+++ simple.py
@@ -1,6 +1,6 @@
 import  os
 # x = input('Enter your name:')
 # x="hair"
-print("Hello ",os.get.environ("GITHUB_SHA"))
+print("Hello ",os.environ.get("GITHUB_SHA"))
 
 #print("Bro this is the value ",os.environ.get("UID"))
