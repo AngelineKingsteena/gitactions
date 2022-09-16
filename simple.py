72db47ee4d13dcbd0a0b5e96e77313f44e959c3e
AK <angelinekingsteena@gmail.com>
2022-09-17 04:36:29 +0530

    changed scripts

--- simple.py
+++ simple.py
@@ -1,9 +1,9 @@
 import  os
 # x = input('Enter your name:')
 # x="hair"
-github_sha=os.environ["GITHUB_SHA"]
+# github_sha=os.environ["GITHUB_SHA"]
 UID=os.environ["ES_UID"]
-print("Hello ",os.environ["GITHUB_SHA"])
+# print("Hello ",os.environ["GITHUB_SHA"])
 folder=UID+"_yoBRO"
 os.mkdir(folder)
 
